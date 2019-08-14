26d278d6a70d774afd3dc198f2b97e8ef383fa84
ejaramillom <ejaramilo@hotmail.com>
2019-08-13 22:13:37 -0500

    Se modifica solutions.rb

--- solution.rb
+++ solution.rb
@@ -1,11 +1,9 @@
-require "sinatra"
+require 'sinatra'
 
-get '/' do
-
-  unless params[:nombre]
-    "<h1> Hola desconocido!</h1>"
-  else
-    "<h1> Hola #{params[:nombre]}! </h1>"
-  end
-
+get '/' do 
+	if params[:nombre].empty?
+		"<h1>Hola desconocido!</h1>"
+	else
+		"<h1> Hola #{params[:nombre]}! </h1>"
+	end
 end
